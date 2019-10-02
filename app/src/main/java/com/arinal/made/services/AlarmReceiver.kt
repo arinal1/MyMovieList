96ae4b50533e60ee65a2715afb171762f9ffdb0a
Muhammad Arinal <arinal.rnl@gmail.com>
2019-10-02 10:16:10 +0700

    refactor-useAlarmSetRepeatingInsteadJobScheduler

--- app/src/main/java/com/arinal/made/services/AlarmReceiver.kt
+++ app/src/main/java/com/arinal/made/services/AlarmReceiver.kt
@@ -3,40 +3,69 @@ package com.arinal.made.services
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build.VERSION.SDK_INT
-import android.os.Build.VERSION_CODES.N
+import com.arinal.made.R
+import com.arinal.made.ReferenceSeeApplication
+import com.arinal.made.data.DataCallback
+import com.arinal.made.data.TmdbRepository
 import com.arinal.made.data.local.PreferenceManager
-import com.arinal.made.services.DailyUpdateService.Companion.showUpdateNotification
-import com.arinal.made.services.ReminderService.Companion.showReminderNotification
-import com.arinal.made.utils.AlarmUtils.Companion.setReminderJob
-import com.arinal.made.utils.AlarmUtils.Companion.setUpdateJob
+import com.arinal.made.data.local.TmdbDatabase
+import com.arinal.made.data.model.FilmModel
+import com.arinal.made.data.network.ApiClient
 import com.arinal.made.utils.Constants
+import com.arinal.made.utils.Constants.reminderServiceId
+import com.arinal.made.utils.Constants.updateServiceId
+import com.arinal.made.utils.NotificationUtils
+import com.arinal.made.utils.scheduler.SchedulerProviderImpl
+import io.reactivex.disposables.CompositeDisposable
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         val alarmType = intent.getIntExtra("alarmId", 0)
         val preference = PreferenceManager(context)
-        if (alarmType == Constants.reminderServiceId) {
-            if (preference.reminderSet) {
-                preference.reminderJobSet = false
-                if (SDK_INT < N) {
-                    val channelId = Constants.reminderChannelId
-                    val channelName = Constants.reminderChannelName
-                    showReminderNotification(context, channelId, channelName)
+        if (alarmType == reminderServiceId && preference.reminderSet) showReminderNotification(context)
+        else if (alarmType == updateServiceId && preference.dailyUpdateSet) showUpdateNotification(context)
+    }
+
+    private fun showReminderNotification(context: Context) {
+        val preference = PreferenceManager(context)
+        val date = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(System.currentTimeMillis())
+        if (preference.lastReminderNotif != date) {
+            ReferenceSeeApplication.setLocale(context)
+            val title = context.getString(R.string.notif_reminder_title)
+            val msg = context.getString(R.string.notif_reminder_msg)
+            val channelId = Constants.reminderChannelId
+            val channelName = Constants.reminderChannelName
+            NotificationUtils.showNotification(context, channelId, channelName, title, msg)
+            preference.lastReminderNotif = date
+        }
+    }
+
+    private fun showUpdateNotification(context: Context) {
+        val preference = PreferenceManager(context)
+        val date = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(System.currentTimeMillis())
+        if (preference.lastUpdateNotif != date) {
+            ReferenceSeeApplication.setLocale(context)
+            val compositeDisposable = CompositeDisposable()
+            TmdbRepository(
+                TmdbDatabase.getInstance(context).tmdbDao(),
+                compositeDisposable, SchedulerProviderImpl(), ApiClient.getTmdb()
+            ).getUpdates(date, object : DataCallback.FilmCallback {
+                override fun onFailed(throwable: Throwable) {}
+                override fun onGotData(category: Int, data: MutableList<FilmModel>) {
+                    for (update in data) {
+                        val title = update.title
+                        val msg = "$title ${context.getString(R.string.released_today)}"
+                        val channelId = Constants.updateChannelId
+                        val channelName = Constants.updateChannelName
+                        NotificationUtils.showNotification(context, channelId, channelName, title, msg)
+                    }
+                    preference.lastUpdateNotif = date
+                    compositeDisposable.dispose()
                 }
-                setReminderJob(context)
-            }
-        } else if (alarmType == Constants.updateServiceId) {
-            if (preference.dailyUpdateSet) {
-                preference.dailyUpdateJobSet = false
-                if (SDK_INT < N) {
-                    val channelId = Constants.updateChannelId
-                    val channelName = Constants.updateChannelName
-                    showUpdateNotification(context, channelId, channelName, {}, {})
-                }
-                setUpdateJob(context)
-            }
+            })
         }
     }
 }
--- app/src/main/java/com/arinal/made/utils/AlarmUtils.kt
+++ app/src/main/java/com/arinal/made/utils/AlarmUtils.kt
@@ -1,26 +1,15 @@
 package com.arinal.made.utils
 
 import android.app.AlarmManager
+import android.app.AlarmManager.INTERVAL_DAY
 import android.app.AlarmManager.RTC_WAKEUP
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.app.PendingIntent.getBroadcast
-import android.app.job.JobInfo.Builder
-import android.app.job.JobInfo.NETWORK_TYPE_ANY
-import android.app.job.JobScheduler
-import android.content.ComponentName
 import android.content.Context
 import android.content.Context.ALARM_SERVICE
-import android.content.Context.JOB_SCHEDULER_SERVICE
-import android.os.PersistableBundle
 import com.arinal.made.data.local.PreferenceManager
 import com.arinal.made.services.AlarmReceiver
-import com.arinal.made.services.DailyUpdateService
-import com.arinal.made.services.ReminderService
-import com.arinal.made.utils.Constants.reminderChannelId
-import com.arinal.made.utils.Constants.reminderChannelName
 import com.arinal.made.utils.Constants.reminderServiceId
-import com.arinal.made.utils.Constants.updateChannelId
-import com.arinal.made.utils.Constants.updateChannelName
 import com.arinal.made.utils.Constants.updateServiceId
 import org.jetbrains.anko.intentFor
 import java.lang.System.currentTimeMillis
@@ -46,35 +35,6 @@ class AlarmUtils {
             }
         }
 
-        fun setReminderJob(context: Context) {
-            val jobService = ComponentName(context, ReminderService::class.java)
-            val extras = PersistableBundle().apply {
-                putString("reminderChannelId", reminderChannelId)
-                putString("reminderChannelName", reminderChannelName)
-            }
-            setJob(context, reminderServiceId, jobService, extras)
-        }
-
-        fun setUpdateJob(context: Context) {
-            val jobService = ComponentName(context, DailyUpdateService::class.java)
-            val extras = PersistableBundle().apply {
-                putString("updateChannelId", updateChannelId)
-                putString("updateChannelName", updateChannelName)
-            }
-            setJob(context, updateServiceId, jobService, extras)
-        }
-
-        private fun setJob(context: Context, jobId: Int, jobService: ComponentName, extras: PersistableBundle) {
-            val builder = Builder(jobId, jobService)
-                .setExtras(extras)
-                .setRequiredNetworkType(NETWORK_TYPE_ANY)
-                .setRequiresDeviceIdle(false)
-                .setRequiresCharging(false)
-                .setPeriodic(24 * 60 * 60 * 1000L)
-            val jobScheduler = context.getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
-            jobScheduler.schedule(builder.build())
-        }
-
         private fun setAlarm(context: Context, hour: Int, alarmId: Int) {
             val calendar = getInstance().apply {
                 set(HOUR_OF_DAY, hour)
@@ -89,7 +49,7 @@ class AlarmUtils {
             val intent = context.intentFor<AlarmReceiver>("alarmId" to alarmId)
             val pendingIntent = getBroadcast(context, alarmId, intent, FLAG_UPDATE_CURRENT)
             val alarmManager = context.getSystemService(ALARM_SERVICE) as AlarmManager
-            alarmManager.setExact(RTC_WAKEUP, time, pendingIntent)
+            alarmManager.setRepeating(RTC_WAKEUP, time, INTERVAL_DAY, pendingIntent)
         }
     }
 }
